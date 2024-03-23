6e3ed93072f19d221efc4a022fddbb2ea5603c96
HelperCode <109080360+Helper-Botz@users.noreply.github.com>
2024-03-23 16:59:21 +0530

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQTheFileDonorBot
+  git clone https://github.com/Helper-Botz/24BOT.git /24BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  git clone $UPSTREAM_REPO /24BOT
 fi
-cd /DQTheFileDonorBot
+cd /24BOT
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
-python3 bot.py
+echo "Starting Bot...."
+python3 lilsa.py
